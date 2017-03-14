27c30604e98f15696a6f026055d4343c1fa66860
Afonso Haruo Carnielli Mukai <afonsoharuo@users.noreply.github.com>
2017-03-14 13:04:17 +0100

    Fix provisioning script

--- provisioning.sh
+++ provisioning.sh
@@ -4,12 +4,12 @@
 cat <<EOF > /etc/yum.repos.d/dm-jenkins.repo
 [dm-jenkins]
 name=Data Management Jenkins
-baseurl=https://jenkins.esss.dk/dm/job/repo-stable/lastSuccessfulBuild/artifact/repo
+baseurl=https://jenkins.esss.dk/dm/job/dm-repo-stable/lastSuccessfulBuild/artifact/repo
 gpgcheck=0
 EOF
 
 yum install -y epel-release
-yum install -y java dm-zookeeper dm-kafka dm-librdkafka-devel
+yum install -y java dm-zookeeper dm-kafka dm-librdkafka-devel python-pip
 
 yum install -y python-pip python-devel
 pip install confluent-kafka
